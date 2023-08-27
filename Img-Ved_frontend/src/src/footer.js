d6accc3e2046f07e842b7a9dc73c35311861b55b
Rama Chandra Rout <ramchandrarout2001@gmail.com>
2023-08-27 12:34:51 +0530

    commit

---
+++ Img-Ved_frontend/src/src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // You can create a corresponding CSS file for styling
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 Aveti Learning. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
